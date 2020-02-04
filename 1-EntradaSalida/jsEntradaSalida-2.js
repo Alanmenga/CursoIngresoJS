7fe98333457ae9a04afd3fb75910be3d8985bb31
alumno <alumno>
2020-02-04 19:12:02 -0300

    Ejercicio 2 resuelto

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -3,7 +3,7 @@ y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
     var nombre;
-    nombre = prompt ("Ingrese su nombre:","-");
-    alert (nombre);
+    nombre = prompt ("Ingrese su nombre: ","-");
+    alert ("Hola " +nombre);
 }
 
