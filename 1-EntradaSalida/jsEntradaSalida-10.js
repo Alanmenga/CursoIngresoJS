eaaf7bcdf775f86c1012a7aa4d5c834b116aadd9
alumno <alumno>
2020-02-04 21:18:14 -0300

    Ejercicios 9 y 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var numeroUno;
+    var numeroDos;
+
+    numeroUno = parseInt(document.getElementById("importe").value);
+
+    numeroDos = numeroUno - numeroUno * 0.25  ;
+
+    document.getElementById("resultado").value = numeroDos ;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+     var numeroUno;
+     var numeroDos;
+     
 
+     numeroUno = parseInt(document.getElementById("sueldo").value);
+
+     numeroDos = numeroUno * 0.1 + numeroUno ;
+     
+     document.getElementById("resultado").value = numeroDos;
+
+    
 	
 }
