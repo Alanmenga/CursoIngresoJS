72ee4cbdfe4e4f8a44fefbe9fa4d27c650e6f14a
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-09 18:03:28 -0300

    Ejercicio 4 resuelto

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
+
+if ( edad < 13)
+
+{ alert ("Usted es menor de edad"); }
+
+ else if ( edad < 18 )
+
+{ alert ("Usted es adolescente"); }
+
+ else if ( edad >= 18)
+
+{ alert ("Usted es mayor de edad"); }
 
 }//FIN DE LA FUNCIÓN
