e82115c94c6618870bc98d0c1e074305b60ff03c
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-10 22:49:55 -0300

    Ejercicio 6 resuelto

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,21 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
+
+if ( edad < 13 )
+
+{ alert ("Usted es un niño"); }
+
+else if ( edad < 18 )
+
+{ alert ("Usted es un adolescente"); }
+
+else if ( edad >= 18 )
+
+{ alert ("Usted es mayor de edad"); }
 
 
 }//FIN DE LA FUNCIÓN
