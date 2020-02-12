bc1bb67639d6e9dcb2155dc3ec8d18c75d5a1a69
alumno <alumno>
2020-02-11 21:18:49 -0300

    Ejercicio 7 resuelto

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -8,11 +8,17 @@ var estadoCivil;
 edad = parseInt(document.getElementById("edad").value);
 estadoCivil = document.getElementById("estadoCivil").value;
 
+console.log("Ver edad :"+ edad);
+console.log("Ver estado civil: "+ estadoCivil);
+
+
 if ( edad < 18  && estadoCivil != "Soltero" )
 { 
     alert ("Es muy pequeño para no estar soltero");
 }
-	
+/*
+ if (!(edad >=18) && !(estadoCivil == "Soltero" ))
+	{alert("Es muy pequeño para no ser soltero");}
 
-
+*/
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,9 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var nro;
 
+nro = Math.floor(Math.random() * 10 + 1) ;
+
+alert ("El numero random es: " + nro);
 }//FIN DE LA FUNCIÓN
