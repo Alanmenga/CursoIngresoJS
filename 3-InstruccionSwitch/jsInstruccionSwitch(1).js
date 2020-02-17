9dcb74d0cd04675d2ec25de73f03cf24d9a854cc
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-17 19:08:02 -0300

    Ejercicio 1 resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,22 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mesDelAño)
+{
+    case "Enero":
+        alert ("Que comiences bien el año");
+        break;
+    case "Marzo":
+        alert ("A clasess!!!");
+        break;
+    case "Julio":
+        alert ("Se vienen las vacaciones");
+        break;
+    case "Diciembre":
+        alert ("Felices fiestas!!!");
+        break;
+}
+
 
 
 
