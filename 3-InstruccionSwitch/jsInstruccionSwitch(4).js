3fd707d35ca3f88dfdb021b5ae530d332d5c03a4
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-17 20:35:15 -0300

    Ejercicio 4 resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,27 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+switch (mesDelAño)
+{
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+        alert ("El mes tiene 31 dias.");
+        break;
+    case "Febrero":
+        alert ("El mes tiene 28 dias");
+        break;
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+     case "Noviembre":
+        alert ("El mes tiene 30 dias");
+        break;
+}
 	
 
 
