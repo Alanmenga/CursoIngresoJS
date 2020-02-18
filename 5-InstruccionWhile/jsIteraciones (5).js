6c4c135cf2c4857cc7992ab05548e84b8548b030
alumno <alumno>
2020-02-18 19:33:01 -0300

    Ejercicio 5 resuelto

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,9 +2,26 @@ function mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
-
-
-
+sexo = sexo.toLocaleLowerCase();
+//Validar el dato. No vamos a parar hasta que el dato este bien
+while (!(sexo == "f" || sexo == "m")) {
+    var sexo = prompt("ingrese f ó m .");
+    sexo = sexo.toLocaleLowerCase();
+}
+/*do {
+     var sexo = prompt("ingrese f ó m .");
+     sexo = sexo.toLocaleLowerCase();
+    } while (!(sexo=="f" || sexo=="m"));
+*/
+//Aca el dato es correcto
+switch (sexo) {
+    case "f":
+        sexo = "Femenino"
+        break;
+    case "m":
+        sexo = "Masculino"
+        break;
+}
 document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
