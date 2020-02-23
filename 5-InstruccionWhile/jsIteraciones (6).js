564c242749e37b381a7f58d0593bc4fdc34727f9
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-23 19:04:49 -0300

    Ejercicio 6 resuelto

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,21 +3,27 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var nroIngresado;
+	var promedio;
 
 do {
+	  //Ingresar el numero y parsearlo
 	  nroIngresado = parseInt(prompt("Ingrese un numero: "));
+	  //Validar que el dato este correcto
 	  while (isNaN(nroIngresado)) {
 		  nroIngresado = prompt ("Lo que ingresaste no era un nro, volve a ingresarlo:");
 		  nroIngresado = parseInt(nroIngresado);
-	      }
-	  contador++;
+		  }
+	  //En esta parte sabemos que lo ingresado es un numero
+	  contador++; //contador = contador + 1
 	  acumulador = nroIngresado + acumulador;
 	  console.log ("El contador es: "+contador);
 	  console.log ("Se encuentra acumulando: "+acumulador);
 	}while (contador<5);
 
-
+//Mostramos el acumulador
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+//Mostramos el promedio
+promedio = acumulador / contador ;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
