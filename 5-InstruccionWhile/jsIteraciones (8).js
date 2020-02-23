354614aa20bd32273e914bb19f1ef62efffadc63
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-23 20:14:19 -0300

    Ejercicio 8 resuelto

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,25 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var nroIngresado;
 	var respuesta='si';
+
+	while ( respuesta == "si"){
+		nroIngresado = parseInt(prompt("Ingresa un nro: "));
+		//validar que el dato este correcto
+		while (isNaN(nroIngresado)){
+			nroIngresado=parseInt(prompt("Lo que ingresaste no es un nro, volve a intentarlo: "));
+		}
+		//En esta parte sabemos que el dato es correcto
+		if ( nroIngresado >= 0 ){
+			positivo = positivo + nroIngresado;
+		}else {
+			negativo = negativo * nroIngresado;
+		}
+		respuesta = prompt ("Desea continuar? Escriba si: ");
+
+
+	}
 
 
 document.getElementById('suma').value=positivo;
