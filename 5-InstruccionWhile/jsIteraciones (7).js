13e404c129440fc012a3e0c8bbc9cd3b61978636
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-23 19:26:04 -0300

    Ejercicio 7 resuelto

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -6,11 +6,19 @@ function mostrar()
 	var respuesta='si';
 	var nroIngresado;
 
-	do {
-		nroIngresado = parseInt(prompt("Ingrese un numero: "));
-		contador++;
-		acumulador = nroIngresado + acumulador;
-	  }while (contador<5)
+	while (respuesta == "si"){
+		nroIngresado =parseInt(prompt("Ingrese un numero"));
+		//validar que el dato este correcto
+		while(isNaN(nroIngresado)){
+			nroIngresado=parseInt(prompt("Lo que ingresaste no era un nro, volve a ingresar: "));
+		}
+		//En esta parte sabemos que el dato es correcto
+		contador++; //contador=contador+1
+		acumulador = acumulador + nroIngresado;
+		console.log ("El contador es: "+contador);
+	    console.log ("Se encuentra acumulando: "+acumulador);
+		respuesta = prompt("Desea continuar? Escriba si para continuar");
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
