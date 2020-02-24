426ace4108be6831f805f5c3a865eca2e9cb83ee
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-02-24 11:58:30 -0300

    Ejercicio 3 resuelto

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,10 @@
 function mostrar()
 {
+    var i; 
+    var repetciones = prompt("ingrese el número de repeticiones");
 
-var repetciones = prompt("ingrese el número de repeticiones");
-
-
+    for (i=1;i<=repetciones;i++){
+        document.write("Hola UTN FRA <br>");
+    }
 
 }//FIN DE LA FUNCIÓN
