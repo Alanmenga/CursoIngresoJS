adbcacb76a33207eeabab0acb65fa297fda28a14
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-03-02 00:39:05 -0300

    Ejercicio 7

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,31 @@
 function mostrar()
 {
+    var i;
+    var nroIngresado;
+    var cantDivisores=0;
 
+    nroIngresado = parseInt(prompt("Ingresa un numero: "));
+    //Validamos
+    while (isNaN(nroIngresado)){
+        nroIngresado = parseInt(prompt("Lo que ingresaste no es un nro, Volve aintentarlo: "));
+    }
+    //Sabemos que lo ingresado es un numero
+    for (i=1; ;i++){
+        var resto;
+        resto = nroIngresado&i;
+        console.log("El resto es: " + resto);
+        if (Number.isFinite(resto)){
+            cantDivisores++;
+            document.write( i + "<br>");
+        }
+        if ( i >= nroIngresado){
+            console.log("i: "+ i + "nro ingresado: " + nroIngresado);
+            break
+
+        }
+
+    }
+    alert ("La cantidad de divisores es: " + cantDivisores);
 
 
 
