aed49cfe8e86881f7b308e34ba049a6ca80e2bea
Alanmenga <alan.gabriel.mengassini@gmail.com>
2020-03-01 23:11:23 -0300

    Ejercicio 6 resuelto

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -4,29 +4,26 @@ function mostrar()
     var nroIngresado;
     var cantPares=0;
    
-
-    
-    for (i=0; ;i++) {
-        nroIngresado = parseInt(prompt ("Ingrese un numero: "));
+    nroIngresado = parseInt(prompt ("Ingrese un numero: "));
         //validamos el dato
         while (isNaN(nroIngresado)){
             nroIngresado=parseInt(prompt("Lo que ingresaste no era un nro, volve a ingresar: "));
         }
         //sabemos que el dato es un numero
     console.log ("El nro ingresado es :"+nroIngresado);
-      var resto=nroIngresado%2;
+    
+    for (i=1; ;i++) {
+        var resto=i%2;
 			if (resto==0){
 		    //es un numero par
-		    cantPares++;
-              }
-    if ( i >= nroIngresado){
-        break
-    }
-    console.log ("La cantidad de pares es: "+cantPares);
+            cantPares++;
+            document.write( i + "<br>"); 
+            }
+        if ( i >= nroIngresado){
+            break
+            }
+        console.log ("La cantidad de pares es: "+cantPares);
     }
     alert ("La cantidad de numeros pares es: "+cantPares);
-
-
-
 
 }//FIN DE LA FUNCIÓN
