be7d781dc5b45f3ea29b008472a40c21b7a1ce1c
alumno <alumno>
2020-03-02 22:12:18 -0300

    Ejercicio 8 parcial 2018 resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+    var letra;
+    var numero;
+    var respuesta="si";
+    var contador=0;
+
+    while(respuesta=="si"){
+        letra=prompt("Ingrese una letra: ");
+        //valido numero
+        while () {
+
+        }
+        numero=parseInt(prompt("Ingrese un numero entre -100 y 100: "));
+        //valido letra
+        while (numero<-100 && numero>100){
+            numero=parseInt(prompt("Numero incorrecto.Ingrese un numero: "));
+        }
+        respuesta = prompt ("Desea continuar? Escriba si: ");
+        contador++;
+    }
+    document.write()
 
 }
